b2011412c499adb2954229d7fdd76bade0de28b4
albe2669 <albert@risenielsen.dk>
2021-09-10 14:10:39 +0200

    Completed Regex

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -4,5 +4,72 @@ namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_given_lorem_ipsum_return_words() {
+            // Arrange
+            var strings = new[] {
+                "Neque porro quisquam",
+                "est qui dolorem",
+                "ipsum quia dolor",
+                "sit amet"
+            };
+
+            // Act
+            var output = RegExpr.SplitLine(strings);
+
+            // Assert
+            Assert.Equal(new[]{"Neque", "porro", "quisquam", "est", "qui", "dolorem", "ipsum", "quia", "dolor", "sit", "amet"}, output);
+        }
+
+        [Fact]
+        public void resolutions_test() {
+            // Arrange
+            var resolutions = new[] {
+                "1920x1080",
+                "1024x768, 800x600, 640x480",
+                "320x200, 320x240, 800x600",
+                "1280x960"
+            };
+
+            // Act
+            var output = RegExpr.Resolution(resolutions);
+
+            // Assert
+            Assert.Equal(new[] {
+                (1920, 1080),
+                (1024, 768),
+                (800, 600),
+                (640, 480),
+                (320, 200),
+                (320, 240),
+                (800, 600),
+                (1280, 960)
+            }, output);
+        }
+
+        [Fact]
+        public void InnerText_given_a_return_data() {
+            var html = "<div><p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+       
+            var output = RegExpr.InnerText(html, "a");
+
+            Assert.Equal(new[]{
+                "theoretical computer science",
+                "formal language",
+                "characters",
+                "pattern",
+                "string searching algorithms",
+                "strings"
+            }, output);
+        }
+
+        [Fact]
+        public void InnerText_given_p_return_data() {
+            var html = "<div><p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p></div>";
+       
+            var output = RegExpr.InnerText(html, "p");
+
+            Assert.Equal(new[] {"The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."}, output);
+        }
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,17 +8,34 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            foreach (var item in lines)
+            {
+                foreach (var word in item.Split(' ')) {
+                    yield return word;
+                }
+            }
         }
 
-        public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+        public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
         {
-            throw new NotImplementedException();
+           var pattern = @"((?<resX>\d+)x(?<resY>\d+))+"; 
+         
+           foreach (var line in resolutions) {
+               foreach (Match match in Regex.Matches(line, pattern))
+               {
+                   yield return (Int32.Parse(match.Groups["resX"].Value), Int32.Parse(match.Groups["resY"].Value));
+               }
+           }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
         {
-            throw new NotImplementedException();
+           var pattern = $@"(<{tag}.*?>(?<innerText>.+?)<\/{tag}>)+";
+
+            foreach (Match match in Regex.Matches(html, pattern))
+            {
+                yield return Regex.Replace(match.Groups["innerText"].Value, @"(<.+?>)|(<\/.+?>)", "");
+            }
         }
     }
 }
